5bc0451ecc723c3205d78eb9d9b206e6f82e58d9
itsCodemaster <36035481+itsCodemaster@users.noreply.github.com>
2018-03-12 22:22:26 +0530

    Update DBIntializer.java

--- onlinebanking/src/g/DBIntializer.java
+++ onlinebanking/src/g/DBIntializer.java
@@ -2,7 +2,7 @@ package g;
 
 public interface DBIntializer {
 String DRIVER="org.postgresql.Driver";
-String CON_STRING="jdbc:postgres://ltfcprxskyvfpe:860637ccd278fcb5b143b807c96a94dc0317f9a0d22f1c62434b21847cf08241@ec2-54-83-23-91.compute-1.amazonaws.com:5432/dffjpeob11jlpu";
+String CON_STRING="postgres://ltfcprxskyvfpe:860637ccd278fcb5b143b807c96a94dc0317f9a0d22f1c62434b21847cf08241@ec2-54-83-23-91.compute-1.amazonaws.com:5432/dffjpeob11jlpu";
 String USERNAME="ltfcprxskyvfpe";
 String PASSWORD="860637ccd278fcb5b143b807c96a94dc0317f9a0d22f1c62434b21847cf08241";
 }
